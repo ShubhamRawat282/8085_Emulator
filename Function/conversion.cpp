f38953d39dcbd02d975e1aae6a5b3d7e3555e80b
Simran0431 <simrann197@gmail.com>
2023-04-28 13:23:59 +0530

    added main

---
+++ Function/conversion.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+int hexadecimalToDecimal(string hexVal)
+{
+    int len = hexVal.size();
+    int base = 1;
+ 
+    int dec_val = 0;
+    for (int i = len - 1; i >= 0; i--) {
+        if (hexVal[i] >= '0' && hexVal[i] <= '9') {
+            dec_val += (int(hexVal[i]) - 48) * base;
+            base = base * 16;
+        }
+        else if (hexVal[i] >= 'A' && hexVal[i] <= 'F') {
+            dec_val += (int(hexVal[i]) - 55) * base;
+            base = base * 16;
+        }
+    }
+    return dec_val;
+}
+
+int main()
+{
+    string hexNum = "1A";
+    cout << (hexadecimalToDecimal(hexNum));
+    return 0;
+}
+
--- main.cpp
+++ main.cpp
@@ -1,9 +1,19 @@
-#include "../HeaderFiles/header.h"
-class Emulator{
+#include "../8085_Emulator/HeaderFiles/header.h"
     private:
         map<string,string>memory;
-        int flag[5];
+        bool flag[5];
         string reg[7];
     public:
-        
+        Emulator()
+        {
+            flag[5]={false};
+            reg[7]={NULL};
+        }
 };
+int main()
+{
+    string pc;
+    cout<<"Enter the address from where you want to start : ";
+    cin>>pc;
+
+}
